588387dac449c83080eb1309a389ff1d7c350d1c
SieunAhn0 <saahn1405@gmail.com>
2026-09-06 06:54:20 -0700

    Modified droplets to adjust difficulty of game
    Now we just need art to be embeded in, and potentially more sound effects/music

--- Assets/Scripts/Droplet Spawner.cs
+++ Assets/Scripts/Droplet Spawner.cs
@@ -13,7 +13,7 @@ public class DropletSpawner : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Invoke("Spawn", 2f);
+        Invoke("Spawn", 1.5f);
     }
 
     void Spawn() {
