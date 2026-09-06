b10bedbc3996610eb235fa5934516f444c84db1b
SieunAhn0 <saahn1405@gmail.com>
2026-09-05 21:40:52 -0700

    Adjusted SFX and looped BGM

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -12,9 +12,22 @@ public class AudioManager : MonoBehaviour
     public AudioClip drop1;
     public AudioClip drop2;
     public AudioClip drop3;
+
+    public float masterSoundVolume = 1;
     
-    private void start() {
+    private void Awake() {
         musicSource.clip = tracks[0];
+        musicSource.volume = masterSoundVolume * 0.7f;
+        musicSource.loop = true;
         musicSource.Play();
     }
+
+    public void PlaySFX(float vol, AudioClip clip) {
+        SFXSource.volume = masterSoundVolume * vol;
+        SFXSource.PlayOneShot(clip);
+    }
 }
+
+// AudioManager audioManager;
+// audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+// audioManager.PlaySFX(audioManager.);
